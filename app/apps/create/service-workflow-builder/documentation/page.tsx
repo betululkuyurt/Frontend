91beb4b0bc95e3c20781fee5e6fd035bf91a6d71
umuts22 <umutsahin55@icloud.com>
2025-06-02 21:43:52 +0300

    service workflow has a documentation button now

---
+++ app/apps/create/service-workflow-builder/documentation/page.tsx
@@ -0,0 +1,8 @@
+"use client"
+
+import ServiceWorkflowDocumentation from "../documentation"
+
+export default function ServiceWorkflowBuilderDocumentationPage() {
+  // Simply render the documentation component
+  return <ServiceWorkflowDocumentation />
+}
